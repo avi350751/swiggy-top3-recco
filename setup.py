78fd30ce9e2a4a4cce636046cc54738f2f49eb3a
Avishek Bhattacharjee <avishek350751@gmail.com>
2023-12-13 18:39:48 -0600

    Cookiecutter initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project will recommend top three food items on swiggy home screen',
+    author='Avishek Bhattacharjee',
+    license='',
+)
